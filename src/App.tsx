e93f42653012425e6b63592b333f829433e851eb
Nayara Holanda <naayaraholanda@gmail.com>
2023-01-19 15:00:32 -0300

    Add project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Habit from './components/Habit'
+import './styles/global.css'
+
+function App() {
+  return (
+    <Habit />
+  )
+}
+
+export default App
---
+++ src/components/Habit.tsx
@@ -0,0 +1,11 @@
+interface HabitProps {
+
+}
+
+function Habit(props: HabitProps) {
+  return (
+    <div className="text-orange-500">Teste</div>
+  )
+}
+
+export default Habit
